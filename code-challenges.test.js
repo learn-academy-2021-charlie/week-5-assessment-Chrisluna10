516f2b70be5c89bfe3200a476ab6cf95811e3590
Chrisluna10 <chrisluna4224@gmail.com>
2021-08-09 08:52:13 -0700

    completed assessment

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -24,11 +24,40 @@ var secretCodeWord2 = "Gobbledygook"
 var secretCodeWord3 = "Eccentric"
 // Expected output: "3cc3ntr1c"
 
+describe("code", () => {
+    test("returns a coded message", () => {
+      expect(code(secretCodeWord1)).toEqual("L4ck4d41s1c4l")
+      expect(code(secretCodeWord2)).toEqual("G0bbl3dyg00k")
+      expect(code(secretCodeWord3)).toEqual("3cc3ntr1c")
+    })
+  })
 
 
 // b) Create the function that makes the test pass.
 
+// create function called letterValue
+// parameter = string
+// create a variable that converts certain letters to numbers using colon to define a property
+//if statement (if the length of the string is 1, return the string with the new assignments from the number variable)
+// return the string with toLowerCase to eliminate any uppercase letters
+//split the string into individual strings to itterate through
+//map through each string
+//join the string back into one.
 
+const letterValue = (string) => {
+    var number={
+        a: 4, e: 3, i: 1, o: 0
+    }
+    if(string.length == 1) return number[string] || string;
+    return string.toLowerCase().split('').map(letterValue).join('');
+    
+}
+
+console.log(letterValue(secretCodeWord1))
+console.log(letterValue(secretCodeWord2))
+console.log(letterValue(secretCodeWord3))
+
+//I was able to get the first and third variable to work but the o was not converting to a 0 for some reason.
 
 // --------------------2) Create a function that takes in an array and returns all the words that contain the letter a.
 
@@ -39,10 +68,25 @@ var arrayOfWords1 = ["Apple", "Banana", "Plum", "Orange", "Kiwi"]
 var arrayOfWords2 = ["Mango", "Cherry", "Apricot", "Blueberry", "Peach"]
 // Expected output: ["Mango", "Apricot", "Peach"]
 
-
+describe("letter", () => {
+    test("returns all the words that contain the letter a", () => {
+      expect(letter(arrayOfWords1)).toEqual(["Apple", "Banana", "Orange"])
+      expect(letter(arrayOfWords2)).toEqual(["Mango", "Apricot", "Peach"])
+    })
+  })
 
 // b) Create the function that makes the test pass.
 
+//create a function called letter that takes in an array
+//filter through the array and lowercase each value and whether or not it includes "a"
+//return that array with value that includes "a"
+
+const letter = (array) => {
+    return array.filter(value => value.toLowerCase().includes("a"))
+        }
+
+console.log(letter(arrayOfWords1))
+console.log(letter(arrayOfWords2))
 
 
 // --------------------3) Create a function that takes in an array of 5 numbers and determines whether or not the array is a “full house”. A full house is exactly one pair and one three of a kind.
@@ -56,6 +100,42 @@ var hand2 = [5, 5, 3, 3, 4]
 var hand3 = [5, 5, 5, 5, 4]
 // Expected output: false
 
+describe("fullHouse", () => {
+    test("takes in an array of 5 numbers and determines whether or not the array is a “full house”.", () => {
+      expect(fullHouse(hand1)).toEqual(true)
+      expect(fullHouse(hand2)).toEqual(false)
+      expect(fullHouse(hand3)).toEqual(false)
+    })
+  })
+
 
 
 // b) Create the function that makes the test pass.
+
+// const fullHouse = (array) => {
+//     for(let i=0; i<array.length; i++){
+//         if(array[i] === )
+//     }
+// }
+
+// create function called letterValue
+// parameter = array
+// create a variable with an empty array to take in the values.
+// The second part of this function was put together from internet sources.
+// I understand most of how this code was put together but I could not find information on like 130 and 132.
+
+const fullHouse = (arr) => {
+    let count = []
+    for(let x of arr){
+      count[x] = (count[x] || 0) + 1;
+    }
+    let value = Object.values(count);
+    if((value[0] === 2 && value[1] === 3) || (value[1] === 2 && value[0] === 3)){
+      return true;
+    }
+    return false;
+  }
+
+  console.log(fullHouse(hand1))
+  console.log(fullHouse(hand2))
+  console.log(fullHouse(hand3))
--- interview-questions.md
+++ interview-questions.md
@@ -15,45 +15,46 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 2. What is a relational database? Are there other kinds of databases?
 
-  Your answer:
+  Your answer: This is the type of database that stores and provides access to data that is related. There are non-relational databases as well.
 
-  Researched answer:
+  Researched answer: Non-relational databases, also called NoSQL databases, the most popular being MongoDB, DocumentDB, Cassandra, Coachbase, HBase, Redis, and Neo4j. 
 
 
 
 3. What is object-relational mapping?
 
-  Your answer:
+  Your answer: Converting data from a database using object oriented programming. 
 
-  Researched answer:
+  Researched answer: Object-Relational Mapping (ORM) is a technique that lets you query and manipulate data from a database using an object-oriented paradigm.
 
 
 
 4. What is a gem?
 
-  Your answer:
+  Your answer: Library with functions, extention.
 
-  Researched answer:
+  Researched answer: a package manager for the Ruby programming language that provides a standard format for distributing Ruby programs and libraries
 
 
 
 5. What are primary keys? Why are they important?
 
-  Your answer:
+  Your answer: The initial column that has a set primary key values. 
 
-  Researched answer:
+  Researched answer: A primary key is a column or a set of columns in a table whose values uniquely identify a row in the table. A relational database is designed to enforce the uniqueness of primary keys by allowing only one row with a given primary key value in a table.
 
 
 
 ## Looking Ahead: Terms for Next Week
 Research and define the following terms to the best of your ability.
 
-1. RESTful routes:
+1. RESTful routes: RESTful routing is a set of standards used in many different languages to create efficient, reusable routes. 
 
-2. JSON:
+2. JSON: JSON (JavaScript Object Notation) is a lightweight data-interchange format. 
 
-3. ERB:
+3. ERB: ERB templates combine plain text with Ruby code for variable substitution and flow control, making them easy to write and maintain.
 
-4. Params:
+4. Params: Params in Ruby on Rails is a hash. It is the collection of data received by the application during a particular HTTP request. 
 
-5. API:
+5. API: 
+API is the acronym for Application Programming Interface, which is a software intermediary that allows two applications to talk to each other.
